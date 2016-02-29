be3c67e2e83a6be51036b6776e225803bd07906e
Li Jinning <928029842@qq.com>
2016-02-29 19:41:07 +0800

    Update index.js

--- en-us/index.js
+++ en-us/index.js
@@ -26,7 +26,7 @@ $(document).on('click', '#create-room', function(){
 	var roomId = 'room_' + timestamp.get().num, roomCode = $.password(4), authNumber = RandomNumber(10);
 	myRootRef.child('codes').once('value', function(data){
 		var a = data.val() || null;
-		if( a === null ){  // codes not found
+		if( a === null ){  // codes don't exist
 			var ary = [roomCode];
 		}else{
 			var ary = JSON.parse( a.array ), num = ary.indexOf(roomCode);
@@ -57,7 +57,7 @@ $(document).on('click', '#create-room', function(){
 	});
 });
 
-// rebuild auth random number
+// create auth random number
 function RandomNumber(num){
     var ary = new Array("0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"), Str = '';
     for ( var i=1; i<=num; i++ ){
